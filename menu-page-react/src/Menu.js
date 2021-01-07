3c75195f1ba2a138a1f0cd7c74572de0597518dd
Chriss Chris <zenskech@outlook.com>
2021-01-07 17:17:08 +0500

    edit

--- menu-page-react/src/Menu.js
+++ menu-page-react/src/Menu.js
@@ -10,8 +10,8 @@ const Menu = ({ items }) => {
             <img src={img} alt={title} className="photo" />
             <div className="item-info">
               <header>
-                <h2>{title}</h2>
-                <h2 className="price">${price}</h2>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
               </header>
               <p className="item-text">{desc}</p>
             </div>
